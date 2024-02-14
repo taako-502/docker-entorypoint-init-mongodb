7de50f2fb1b0bfd6d0f339573ef62a0426064dcc
taako-502 <i.am.armorknight@gmail.com>
2024-02-14 20:29:26 +0200

    init

---
+++ docker-entrypoint-initdb.d/1-init.js
@@ -0,0 +1,16 @@
+db = db.getSiblingDB("testdb")
+db.users.drop()
+db.users.insertMany([
+  {
+    name: "John Doe",
+    age: 30,
+    email: "john.doe@example.com",
+  },
+  {
+    name: "Jane Doe",
+    age: 25,
+    email: "jane.doe@example.com",
+  },
+])
+
+print("Users collection created and populated with initial data.")
